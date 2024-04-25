f828d22504814e3ef6bf369c4f47d6dab7b1b97d
Jono Hawkins <hawkinsj22@cardiff.ac.uk>
2024-04-25 12:09:01 +0100

    adds MIT License to datalogger-2024.ino/main.cpp and resets sensor ID

--- firmware/datalogger-2024/src/main.cpp
+++ firmware/datalogger-2024/src/main.cpp
@@ -1,4 +1,28 @@
+/*****************************************************************************
 
+MIT License
+
+Copyright (c) 2024 Cardiff University / cryoskills.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*****************************************************************************/
 
 /* ---------------- USER CONFIGURATION ---------------- */
 
@@ -22,9 +46,9 @@
   or not.
 */
 // Temperature mode for Wheastone bridge
-#define TEMP_MODE_WHEATSTONE
+// #define TEMP_MODE_WHEATSTONE
 // Temperature mode for potential divider
-// #define TEMP_MODE_POTENTIAL
+#define TEMP_MODE_POTENTIAL
 
 /* ---------------- PIN CONFIGURAITON ---------------- */
 /* 
@@ -288,6 +312,7 @@ float_t get_ds18b20_temperature() {
     // and return value when we have it
     return my_ds18b20.getTempC();
   }
+  return 0;
 
 }
 
